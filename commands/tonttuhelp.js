c446286643ccf99a787a738a386a7564236fe362
SteelForce <36268348+SteelForce@users.noreply.github.com>
2020-09-23 13:23:19 +0300

    Create tonttuhelp.js

---
+++ commands/tonttuhelp.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'tonttuhelp',
+    description: "homo",
+    execute(message, args){
+        message.channel.send('commands: !tonttufeedaa, !tonttupallo, !ping, !tonttuhelp');
+    }
+}
